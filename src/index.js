55c4f44ef5d427fe7de09b75df8ceae36aecf59c
fhdyt <fhidayat2206@gmail.com>
2020-08-06 15:21:36 +0700

    6 agustus

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
 const bodyParser = require('body-parser');
+const requireAuth = require('./middlewares/requireAuth');
+
 const app = express();
-const PORT = process.env.PORT || 3000
+
 
 app.use(bodyParser.json());
-app.use(authRoutes);
-app.use(profileRoutes);
+
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req,res) => {
     res.send('Hello World');
 });
+
+app.use(authRoutes);
+app.use(profileRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server Running... ${PORT}`);
